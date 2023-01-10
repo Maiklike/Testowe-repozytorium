811cb39a6c6674c22c32fd87bcdcd990c09c1914
Konrad <konradlewandowski5@protonmail.com>
2023-01-10 15:41:59 +0100

    Pobieranie danych od użytkownika

---
+++ app/src/Student.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Student {
+
+private String firstName;
+private String lastName;
+private int gradeYear;
+private String studentID;
+private String courses;
+private int tuitionBalance;
+private static int costOfCourse = 600;
+private static int id = 1000;
+
+    // Constructor: prompt user to enter student's name and year
+public Student() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter student first name: ");
+    this.firstName = scanner.nextLine();
+
+    System.out.print("Enter student last name: ");
+    this.lastName = scanner.nextLine();
+
+    System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
+    this.gradeYear = scanner.nextInt();
+
+    setStudentID();
+
+    System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+
+}
+
+    // Generate an ID
+private void setStudentID() {
+    // Grade level + ID
+    id++;
+    this.studentID = gradeYear + "" + id;
+}
+
+    // Enroll in courses
+
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
+
+
+}
