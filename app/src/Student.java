6db939de35d6f6c27dae3824a7524f6734cb0468
Konrad <konradlewandowski5@protonmail.com>
2023-01-12 23:26:57 +0100

    Ukończony program

--- app/src/Student.java
+++ app/src/Student.java
@@ -6,8 +6,8 @@ private String firstName;
 private String lastName;
 private int gradeYear;
 private String studentID;
-private String courses;
-private int tuitionBalance;
+private String courses = "";
+private int tuitionBalance = 0;
 private static int costOfCourse = 600;
 private static int id = 1000;
 
@@ -24,10 +24,6 @@ public Student() {
     this.gradeYear = scanner.nextInt();
 
     setStudentID();
-
-    System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
-
-
 }
 
     // Generate an ID
@@ -38,13 +34,44 @@ private void setStudentID() {
 }
 
     // Enroll in courses
+    public void enroll() {
+        // Get inside a loop, user hits 0
+        do {
+            System.out.print("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n  " + course;
+                tuitionBalance += costOfCourse;
+            } else break;
+        } while (true);
+    }
 
 
     // View balance
+public void viewBalance() {
+    System.out.println("Your balance is: $" + tuitionBalance);
+}
 
     // Pay tuition
+public void payTuition() {
+    viewBalance();
+    System.out.print("Enter your payment: $");
+    Scanner in = new Scanner(System.in);
+    int payment = in.nextInt();
+    tuitionBalance -= payment;
+    System.out.println("Thank you for your payment of $" + payment);
+    viewBalance();
+}
 
     // Show status
+    public String showInfo() {
+    return "Name: " + firstName + " " + lastName +
+            "\nGrade Level: " + gradeYear +
+            "\nStudent ID: " + studentID +
+            "\nCourses Enrolled: " + courses +
+            "\nBalance: $" + tuitionBalance;
+    }
 
 
 }
